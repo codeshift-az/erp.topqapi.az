e8c78411d86526985f63b884c950fdf44c0c106a
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-02-27 14:50:50 +0100

    Category Create, Update and Delete added

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/store";
 
 // Reactstrap
 import { Container } from "reactstrap";
 
 // Components
 import Breadcrumbs from "@/components/Breadcrumb";
+import VerifyModal from "@/components/VerifyModal";
 
 // Helpers
 import { getPageTitle } from "@/helpers";
 
+// Types
+import { Category } from "@/types";
+
+// Actions
+import { createCategory, deleteCategory, updateCategory } from "@/store/actions";
+
 // Related components
 import TableContainer from "./components/TableContainer";
+import FormModal from "./components/FormModal";
 
 const Categories = () => {
   const title = "Kateqoriyalar";
 
   document.title = getPageTitle(title);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const { status } = useSelector((state: RootState) => state.category);
+
+  // Selected item
+  const [item, setItem] = useState<Category | null>(null);
+
+  // Form Modal
+  const [formModal, setFormModal] = useState<boolean>(false);
+
+  const onCreate = () => {
+    setItem(null);
+    setFormModal(true);
+  };
+
+  const onUpdate = (data: Category) => {
+    setItem(data);
+    setFormModal(true);
+  };
+
+  const handleSubmit = (formData: FormData) => {
+    if (item !== null) {
+      dispatch(updateCategory({ id: item!.id, data: formData }));
+    } else {
+      dispatch(createCategory(formData));
+    }
+  };
+
+  // Delete Modal
+  const [deleteModal, setDeleteModal] = useState<boolean>(false);
+
+  const onDelete = (data: Category) => {
+    setItem(data);
+    setDeleteModal(true);
+  };
+
+  const handleDelete = () => {
+    if (item) dispatch(deleteCategory(item.id));
+    setDeleteModal(false);
+  };
 
   return (
     <React.Fragment>
@@ -27,7 +79,26 @@ const Categories = () => {
           />
 
           {/* Render Table Container */}
-          <TableContainer />
+          <TableContainer onCreate={onCreate} onUpdate={onUpdate} onDelete={onDelete} />
+
+          {/* Render Form Modal */}
+          <FormModal
+            data={item}
+            show={formModal}
+            isEdit={item !== null}
+            toggle={() => setFormModal(false)}
+            handleSubmit={handleSubmit}
+          />
+
+          {/* Render Delete Modal */}
+          <VerifyModal
+            status={status}
+            show={deleteModal}
+            onVerify={handleDelete}
+            action={deleteCategory.typePrefix}
+            onClose={() => setDeleteModal(false)}
+            message="Seçilmiş məlumatı silmək istədiyinizə əminsiniz?"
+          />
         </Container>
       </div>
     </React.Fragment>
